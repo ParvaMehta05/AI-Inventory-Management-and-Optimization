092cc474c2df058f737cbbc0e1c03868b0b5102c
kushpatel290 <kushp364@gmail.com>
2026-08-17 13:34:18 +0530

    Fix environment file path

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     class Config:
-        env_file = ".env"
+        env_file = "../.env"
 
 
 settings = Settings()
